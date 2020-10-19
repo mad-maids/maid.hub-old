ecd6621516ad755c6ae752b4d2e7dcedfdcb3955
Genemator <genemators@gmail.com>
2020-10-20 04:11:34 +0500

    Update Registry.tsx

--- components/Registry.tsx
+++ components/Registry.tsx
@@ -9,7 +9,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import FileDisplay from "./FileDisplay";
 import { DirEntry } from "../util/registries";
-import { CopyToClipboard } from "react-copy-to-clipboard";
+import CopyToClipboard from "react-copy-to-clipboard";
 import { RawCodeBlock } from "./CodeBlock";
 import Markdown from "./Markdown";
 import { id } from "../id.json";
