b81489f5a8c243341c4f663518f2aaabf1871c60
Genemator <genemators@gmail.com>
2020-10-20 05:06:32 +0500

    lock chaining

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,6 @@ export default function Home() {
         <div className="flex justify-center items-center">
           <svg viewBox="0 0 160 160" width="450" height="450">
             <circle cx="80" cy="80" r="50" />
-            <circle cx="80" cy="80" r="40" fill="#ffffff" />
             <g transform=" matrix(0.866, -0.5, 0.25, 0.433, 80, 80)">
               <path
                 d="M 0,70 A 65,70 0 0,0 65,0 5,5 0 0,1 75,0 75,70 0 0,1 0,70Z"
@@ -45,8 +44,26 @@ export default function Home() {
               transform="matrix(0.866, -0.5, 0.5, 0.866, 80, 80)"
               fill="#000000"
             />
-            <circle cx="53" cy="67" r="7" fill="#ffffff" />
-            <circle cx="83" cy="53" r="7" fill="#ffffff" />
+            <g transform=" matrix(0.866, 0.5, 0.25, 0.433, 80, 80)">
+              <path
+                d="M 0,70 A 65,70 0 0,0 65,0 5,5 0 0,1 75,0 75,70 0 0,1 0,70Z"
+                fill="#8c8c8c"
+              >
+                <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  from="360 0 0"
+                  to="0 0 0"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+              </path>
+            </g>
+            <path
+              d="M 50,0 A 50,50 0 0,0 -50,0Z"
+              transform="matrix(0.866, 0.5, -0.5, 0.866, 80, 80)"
+              fill="#000000"
+            />
           </svg>
         </div>
         <div className="flex mb-4 justify-center">
