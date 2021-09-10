c86f634b793e12fb27468540ca0654c0d41f5465
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-09-11 03:46:35 +0500

    Desktop Registry created

---
+++ pages/xd/[...rest].tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import DesktopRegistry from "../../components/DesktopRegistry";
+
+function RegistryPage(): React.ReactElement {
+  return <DesktopRegistry />;
+}
+
+export default RegistryPage;
---
+++ pages/xd/[identifier]/index.tsx
@@ -0,0 +1,11 @@
+/* Copyright 2020 Genemator Sakhib. All rights reserved. MPL-2.0 license. */
+
+import React from "react";
+
+import DesktopRegistry from "../../../components/DesktopRegistry";
+
+const RegistryPage = (): React.ReactElement => {
+  return <DesktopRegistry />;
+};
+
+export default RegistryPage;
